09aa986243f13cd01e115d806817efe4edd46e75
AuntieDot <yourbesttechnicalchoice@gmail.com>
2022-11-01 19:53:36 -0700

    Update setup.sh
    update adding the portainer lines

--- setup.sh
+++ setup.sh
@@ -21,8 +21,12 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-docker --version 
+sudo docker --version 
 
-docker compose version
+sudo docker compose version
+
+sudo docker volume create portainer_data
+
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
 echo "Finished"
